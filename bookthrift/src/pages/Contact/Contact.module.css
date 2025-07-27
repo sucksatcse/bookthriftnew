a3c11c1a4bc342f48cacfa28e8a14f52fc0a68ff
tasnimap <tasnimafarukprpaty@gmail.com>
2025-07-27 21:43:55 +0600

    Donation

---
+++ bookthrift/src/pages/Contact/Contact.module.css
@@ -0,0 +1,12 @@
+import React from 'react';
+import DonationForm from '../../components/DonationForm/DonationForm';
+
+function Donation() {
+  return (
+    <div style={{ padding: '40px 0' }}>
+      <DonationForm />
+    </div>
+  );
+}
+
+export default Donation;
