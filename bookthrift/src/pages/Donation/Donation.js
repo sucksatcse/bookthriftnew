fcc60c93c02ac3ba16855a020c1c3267895e46af
sucksatcse <tanjim.cse.20230104036@aust.edu>
2025-08-25 21:51:09 +0600

    donationform done

--- bookthrift/src/pages/Donation/Donation.js
+++ bookthrift/src/pages/Donation/Donation.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
 import DonationForm from '../../components/DonationForm/DonationForm';
 
-function Donation() {
+
+
+const Donation = () => {
   return (
-    <div style={{ padding: '40px 0' }}>
+    <div style={styles.container}>
+      <h2 style={styles.heading}>📚 Donate a Book</h2>
+      <p style={styles.text}>
+        Fill out the form below to donate your book and help others discover it.
+      </p>
       <DonationForm />
     </div>
   );
-}
+};
 
-export default Donation;
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "20px",
+  },
+  heading: {
+    fontSize: "24px",
+    marginBottom: "10px",
+  },
+  text: {
+    fontSize: "16px",
+    marginBottom: "20px",
+  },
+};
+
+export default Donation;
