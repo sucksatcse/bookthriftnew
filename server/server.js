448a87ef943b23ffe890ae91104e32a164647286
sucksatcse <tanjim.cse.20230104036@aust.edu>
2025-09-06 20:42:37 +0600

    Added server files for auth

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 import donationRoutes from './routes/donationRoutes.js';
+import userRoutes from './routes/userRoutes.js'; // NEW
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/donations", donationRoutes);
+app.use("/api/user", userRoutes); // NEW
 
 // MongoDB Connection
 mongoose
@@ -26,4 +29,4 @@ mongoose
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // fixed backticks
